b483fe39af0ef974c3837dba9ba94754fe51f071
Irina Tarelkina <itarelkina@yahoo.ca>
2019-04-27 17:17:35 +0000

    Lesson 10: Support Updating Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,17 @@
 class TasksController < ApplicationController
     def index
-        render json: Task.all
+      render json: Task.all
     end
-end
+  
+    def update
+      task = Task.find(params[:id])
+      task.update_attributes(task_params)
+      render json: task
+    end
+  
+    private
+  
+    def task_params
+      params.require(:task).permit(:done)
+    end
+  end
